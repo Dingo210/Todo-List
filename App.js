baca9e38857e662faae68dcd2efd5f744d1a07ee
Eric Domingo <drummer1986@gmail.com>
2019-05-27 10:22:40 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 import './App.css'
+import todo from './todo';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <todo />
+      <div className="todo">
+        <div className="header">
+          <form>
+            <input placeholder="Task" />
+            <button type="submit"> Add Task </button>
+          </form>
+        </div>
       </div>
     )
   }
